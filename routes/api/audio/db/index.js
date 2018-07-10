e8c094885a2a524ce6e03ed18ae9eb63e8c92f76
zou12e <281933726@qq.com>
2018-07-10 23:53:21 +0800

    init

--- routes/api/audio/db/index.js
+++ routes/api/audio/db/index.js
@@ -17,7 +17,9 @@ const Helper = {
     async getAudiosByType (type) {
         let sql = 'select * from audio where type = ? and status = 1';
         sql = mysql.format(sql, type);
+        console.log(sql);
         const result = await mydb.dataCenter(sql).catch(e => []);
+        console.log(result);
         return result;
     }
 };
