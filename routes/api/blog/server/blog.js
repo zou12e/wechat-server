ee6bbe5b8b93b3a0a1b1e33b1c9c5bb9e0d57966
zourunze <281933726@qq.com>
2020-09-23 12:28:06 +0800

    feat:err

--- routes/api/blog/server/blog.js
+++ routes/api/blog/server/blog.js
@@ -7,6 +7,7 @@ const gm = require('gm').subClass({imageMagick: true});
 const db = require('../db');
 const host = config.get('host');
 const saveFolder = '/opt/www/website/static/wechat/images2';
+const logger = require('../../../../middleware/logger');
 const qiniu = require('qiniu');
 const accessKey = 'iyoo6yHtN_euHKaHBpRjlcnC8flcQxGzQhFV8keM';
 const secretKey = 'oS3oX6SXINTp-Nw_Vqq56ZemcN0yUmF9PZG8c36m';
@@ -116,67 +117,71 @@ const Service = {
      * 保存言值图片
      */
     async saveImage (req, res, next) {
-        const info = req.body;
-        const _ph = path.join(__dirname, '../../../../sources');
-        const _font = `${_ph}/PingFang.ttc`;
-        const _bg = `${_ph}/icon-score-${info.random}-jpg.jpg`;
-        const _star = `${_ph}/icon-score-star${info.star}.png`;
-        const _bottom = `${_ph}/icon-score-bottom.png`;
-        const _gw = `${_ph}/icon-score-${parseInt(info.blog.score / 10)}.png`;
-        const _sw = `${_ph}/icon-score-${info.blog.score % 10}.png`;
-        const _white = `${_ph}/icon-score-white.png`;
-        const _head = info.blog.avatarUrl;
-        const _headPath = `${saveFolder}/heads/${info.blog.userId}.jpg`;
-        const _name = `${Date.now()}.jpg`;
-        const _path = `${saveFolder}/${_name}`;
+        try {
+            const info = req.body;
+            const _ph = path.join(__dirname, '../../../../sources');
+            const _font = `${_ph}/PingFang.ttc`;
+            const _bg = `${_ph}/icon-score-${info.random}-jpg.jpg`;
+            const _star = `${_ph}/icon-score-star${info.star}.png`;
+            const _bottom = `${_ph}/icon-score-bottom.png`;
+            const _gw = `${_ph}/icon-score-${parseInt(info.blog.score / 10)}.png`;
+            const _sw = `${_ph}/icon-score-${info.blog.score % 10}.png`;
+            const _white = `${_ph}/icon-score-white.png`;
+            const _head = info.blog.avatarUrl;
+            const _headPath = `${saveFolder}/heads/${info.blog.userId}.jpg`;
+            const _name = `${Date.now()}.jpg`;
+            const _path = `${saveFolder}/${_name}`;
 
-        /**
-         * 不存在头像先保存头像
-         */
-        const result = await saveHead(_head, _headPath).catch(err => {
-            console.log(err);
-            return res.error('save blogImage fail');
-        });
-        if (result) {
-            info.blog.text = repalceB(info.blog.text);
-            gm()
-                .in('-page', '+0+0')
-                .in(_bg)
-                .in('-page', '+0+1159')
-                .in(_bottom)
-                .in('-page', '+188+688')
-                .in(_star)
-                .in('-page', '+252+448')
-                .in(_gw)
-                .in('-page', '+386+448')
-                .in(_sw)
-                .in('-page', '+62+880')
-                .in(_white)
-                .in('-page', '+93+905')
-                .in(_headPath)
-                .fontSize(30)
-                .fill('#ffffff')
-                .font(_font)
-                .drawText(106, 420, '经过“趣朗读”人工智能测评，你的言值分')
-                .fontSize(30)
-                .drawText(parseInt((750 - (750 / 25) * info.blog.text.length) / 2), 810, `${info.blog.text}`)
-                .fill('#999999')
-                .fontSize(28)
-                .drawText(86, 1026, '连续打卡')
-                .drawText(284, 1026, '累计打卡')
-                .drawText(486, 1026, '言值超过全国')
-                .fill('#000000')
-                .drawText(101, 1086, `${info.continuDays}天`)
-                .drawText(317, 1086, `${info.punchDays}次`)
-                .drawText(530, 1086, `${info.blog.percent || 70}%人`)
-                .drawText(164, 940, `${filteremoji(info.blog.nickName)}`)
-                .mosaic()
-                .write(_path, err => {
-                    if (err) {
-                        return res.error('save blogImage fail');
-                    }
-                    res.success(`${host}/static/wechat/images2/${_name}`);
-                });
+            /**
+             * 不存在头像先保存头像
+             */
+            const result = await saveHead(_head, _headPath).catch(err => {
+                console.log(err);
+                return res.error('save blogImage fail');
+            });
+            if (result) {
+                info.blog.text = repalceB(info.blog.text);
+                gm()
+                    .in('-page', '+0+0')
+                    .in(_bg)
+                    .in('-page', '+0+1159')
+                    .in(_bottom)
+                    .in('-page', '+188+688')
+                    .in(_star)
+                    .in('-page', '+252+448')
+                    .in(_gw)
+                    .in('-page', '+386+448')
+                    .in(_sw)
+                    .in('-page', '+62+880')
+                    .in(_white)
+                    .in('-page', '+93+905')
+                    .in(_headPath)
+                    .fontSize(30)
+                    .fill('#ffffff')
+                    .font(_font)
+                    .drawText(106, 420, '经过“趣朗读”人工智能测评，你的言值分')
+                    .fontSize(30)
+                    .drawText(parseInt((750 - (750 / 25) * info.blog.text.length) / 2), 810, `${info.blog.text}`)
+                    .fill('#999999')
+                    .fontSize(28)
+                    .drawText(86, 1026, '连续打卡')
+                    .drawText(284, 1026, '累计打卡')
+                    .drawText(486, 1026, '言值超过全国')
+                    .fill('#000000')
+                    .drawText(101, 1086, `${info.continuDays}天`)
+                    .drawText(317, 1086, `${info.punchDays}次`)
+                    .drawText(530, 1086, `${info.blog.percent || 70}%人`)
+                    .drawText(164, 940, `${filteremoji(info.blog.nickName)}`)
+                    .mosaic()
+                    .write(_path, err => {
+                        if (err) {
+                            return res.error('save blogImage fail');
+                        }
+                        res.success(`${host}/static/wechat/images2/${_name}`);
+                    });
+            }
+        } catch (e) {
+            logger.infos(`saveImage err e:${e}`);
         }
     },
     /**
